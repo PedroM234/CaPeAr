288dede206a583c2b5db2da5b43f6a0404b2b6d3
Pedro Aires Fischer Maciel <20241144010018@ifrn.local>
2025-02-18 10:06:47 -0300

    add python

---
+++ capear/menus.py
@@ -0,0 +1,14 @@
+menu_principal = """""\
+    =======================================================
+    animação de Pedro Maciel, Arthur David e Caio Damasceno
+    =======================================================
+
+    Listagem de cenas
+
+    1. Cena 1
+    2. Cena 2
+    3. Cena 3
+    4. Cena 4
+    5. sair
+
+    """
---
+++ main.py
@@ -0,0 +1,19 @@
+"""a main, irmão, a main"""
+
+from capear.menus import menu_principal
+
+def cena1():
+    """bola"""
+    pass
+
+def cen2():
+    pass
+
+def cena3():
+    pass
+
+def main():
+    op = int(input(menu_principal))
+    while op != 5:
+        op = int(input(menu_principal))
+        
