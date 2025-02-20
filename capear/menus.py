9f4007e68c5079d29b20c39e3f91c8f2fd5df7d9
Pedro Maciel <aires.pedro@escolar.ifrn.edu.br>
2025-02-20 12:33:51 +0000

    while

--- capear/menus.py
+++ capear/menus.py
@@ -1,4 +1,6 @@
-menu_principal = """""\
+import time 
+prompt = input("Qual cena você deseja ver? \n")
+menu_principal = """\
     =======================================================
     animação de Pedro Maciel, Arthur David e Caio Damasceno
     =======================================================
@@ -12,3 +14,11 @@ menu_principal = """""\
     5. sair
 
     """
+
+print(menu_principal)
+
+while prompt != 5:
+    print("Digite uma opção dentre as 5.")
+    time.sleep(1.5)
+    print(menu_principal)
+    
